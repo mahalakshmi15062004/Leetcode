0e9d10d1c553e57c58bf2565dfc55c45fc6ed8c4
Mahalakshmi S <mahaappu125@gmail.com>
2026-03-31 09:22:01 +0530

    Create swapPairs.java

---
+++ mediam/swapPairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // Dummy node to simplify edge cases
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            // Swapping
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            // Move prev forward
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+}
