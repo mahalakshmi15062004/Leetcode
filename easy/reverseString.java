afade93d53eec99dc70c4ea026136804e75145de
Mahalakshmi S <mahaappu125@gmail.com>
2026-08-05 10:41:12 +0530

    Implement reverseString method in Solution class

---
+++ easy/reverseString.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        
+        // Move pointers towards the middle, swapping elements
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            left++;
+            right--;
+        }
+    }
+}
