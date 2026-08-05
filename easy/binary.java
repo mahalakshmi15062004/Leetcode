29af0d861160f4863676f96052fe825d22539594
Mahalakshmi S <mahaappu125@gmail.com>
2026-08-05 10:01:29 +0530

    Add binary search implementation in binary.java
    Implement binary search algorithm to find target index.

---
+++ easy/binary.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int ans=-1;
+       int low=0;
+       int high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+        if(nums[mid] == target){
+            ans=mid;
+            break;
+        }
+        else if(nums[mid]> target){
+            high=mid-1;
+        }
+        else{
+            low=mid +1;
+        }
+        }
+        if(target==-1){
+            return ans;
+        }
+        else{
+            return ans;
+        }
+
+        }
+        }
